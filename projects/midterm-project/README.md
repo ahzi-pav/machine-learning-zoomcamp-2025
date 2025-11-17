4e272b3383eeca74d4dc9963e151eb588ca41df3
GD-Villanueva <paulahzi25@gmail.com>
2025-11-17 16:19:32 +0800

    Add partial midterm project

---
+++ projects/midterm-project/README.md
@@ -0,0 +1,15 @@
+## Credit Scoring Project
+
+Overview:
+
+This is a credit scoring project that features main steps learned from the Machine Learning Zoomcamp lessons until 6th week. 
+The project includes basic data preparation, exploratory analysis, and machine learning.
+
+Why I chose this project?
+
+This is mainly because I will soon deal with a lot of this type of problems in my current job. I am currently Data Analyst/Scientist that was tasked to handle ECl Modelling. This will be a huge help to actually enhance my skills to be better at my job. Appologies as this project is kind-off similar to the example project on 6th week.
+
+Model used: XGBoost
+
+How to use?
+
---
+++ projects/midterm-project/predict.py
@@ -0,0 +1,32 @@
+import pickle
+import pandas as pd
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file = 'model_lr=0.1_md=7'
+
+with open(model_file, 'rb') as f_in:
+
+    model = pickle.load(f_in)
+
+app = Flask('credit_score')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    borrower = request.get_json()
+
+    X = pd.DataFrame([borrower])
+    score = model.predict_proba(X)[:,1]
+    default = model.predict(X)
+
+    result = {
+        'default_probability': float(score),
+        'default': bool(default)
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    
+    app.run(debug=True, host='0.0.0.0', port=9696)
